8898709a5f84b8d2d13ee4d4ce93904840dca393
Jon Turner <jonathan-turner@pluralsight.com>
2018-03-02 14:28:27 -0700

    Initial code commit

---
+++ NetFrameworkConfigurationBuilderTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace NetFrameworkConfigurationBuilderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            var tempConfig = builder.Build();
+            builder.AddJsonFile(tempConfig["CredsFilePath"]); //This part is a little different than .NET Core
+
+            var config = builder.Build();
+            foreach (var section in config.GetChildren())
+            {
+                Console.WriteLine($"{section.Key} : {section.Value}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
